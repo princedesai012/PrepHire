597678da94e585b6bc0ebe986abf1f1e1d32b2f8
PD <ppatel3735@gmail.com>
2025-08-21 18:17:40 +0530

    UPDATE MODEL

--- backend/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-    MONGO_URI = os.getenv("MONGO_URI")
-    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
--- frontend/src/lib/utils.js
+++
@@ -1,6 +0,0 @@
-import { clsx } from "clsx"; // 'type ClassValue' is a type-only import, so it's removed
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs) { // Removed ': ClassValue[]' type annotation
-  return twMerge(clsx(inputs));
-}queueMicrotask
